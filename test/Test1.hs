d46a6f69faf1deac9dec8de52f608f2f2e67e961
Padma Pasupathi <padmagitha@gmail.com>
2019-07-08 11:42:20 -0400

    Test1 Updates

--- test/Test1.hs
+++ test/Test1.hs
@@ -40,3 +40,17 @@ spec :: Spec
 spec = describe "Simplify spec" $ do
           specify "simplify scalar one zero" $ do
               x `shouldBe` x
+              --simplify ((x+y)*(x+y))  `shouldBe` (((const 2.0)*.(x*y))+(x^2)+(y^2))
+              --simplify (((x+y)*(x+y))*(x+y))  `shouldBe` (((const 3.0)*.(y*(x^2)))+((const 3.0)*.(x*(y^2)))+(x^3)+(y^3))
+              simplify (x*x) `shouldBe` (x^2)
+              simplify ((x*x)*x) `shouldBe` (x^3)
+              simplify (const 1/x) `shouldBe` (x^(-1))
+              simplify (x*x/x) `shouldBe` x
+              simplify (x/x) `shouldBe` const 1
+              simplify (x/x/x) `shouldBe` x^(-1) --FIXME
+              simplify (x/x/y) `shouldBe` (y^(-1)) --((x*(x^-1))*(y^-1))
+
+              --simplify (x/x*x) `shouldBe` x^(-1) --FIXME
+              --simplify (x/(x*x)) `shouldBe` (x*((x*x)^(-1))) -- FIXME
+              --simplify ((x*x)/y) `shouldBe` ((y^(-1))*(x^2))
+
