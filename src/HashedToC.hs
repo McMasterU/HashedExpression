c0fc5b2a4469beadbfe772d926715819837eb853
Nhan Thai (dandoh) <dainhan605@gmail.com>
2019-07-08 23:51:40 -0400

    done mem offset and make mem map

--- src/HashedToC.hs
+++ src/HashedToC.hs
@@ -3,12 +3,20 @@
 module HashedToC where
 
 import Data.Graph
+import Data.IntMap.Strict (IntMap)
 import qualified Data.IntMap.Strict as IM
-import Data.List (foldl')
+import Data.List (foldl', tails)
 import Data.Maybe (catMaybes, mapMaybe)
 import Data.Set (Set, empty, insert, member)
 import qualified Data.Set as Set
-import HashedExpression hiding ((-))
+import HashedExpression
+    ( ET(..)
+    , Expression(..)
+    , ExpressionMap
+    , Node
+    , NumType
+    , Shape
+    )
 import HashedHash
 import HashedInner
 import HashedNode
@@ -19,36 +27,12 @@ import HashedUtils
 (<<-) :: String -> String -> String
 (<<-) = (++)
 
--- | Slow topological sort
---
-topologicalSort1 :: (ExpressionMap, Int) -> [Int]
-topologicalSort1 (mp, n) = fst $ dfs n empty
-  where
-    dfs :: Int -> Set Int -> ([Int], Set Int)
-    dfs u seen =
-        let vs = filter (not . flip member seen) . nodeArgs $ retrieveNode u mp -- children that are not seen
-            -- fold init, empty list and list of seen node
-            init :: ([Int], Set Int)
-            init = ([], seen)
-            f :: ([Int], Set Int) -> Int -> ([Int], Set Int)
-            f (ns, visited) v =
-                let (xs, moreVisited) = dfs v visited
-                 in (ns ++ xs, moreVisited)
-            -- fold over
-            (children, childrenSeen) = foldl' f init vs
-         in (children ++ [u], insert u childrenSeen)
-
 -- |
 --
-expressionEdges :: (ExpressionMap, Int) -> [(Int, Int)]
-expressionEdges (mp, n) = Set.toList $ edges n
-  where
-    edges :: Int -> Set (Int, Int)
-    edges nId =
-        let args = nodeArgs $ retrieveNode nId mp
-            thisNode = Set.fromList . map (nId, ) $ args
-         in Set.unions $ thisNode : map edges args
+sizeDouble = 8
 
+-- | Topological sort the expression map, all the dependencies will appear before the depended node
+--
 topologicalSort :: (ExpressionMap, Int) -> [Int]
 topologicalSort expr@(mp, n) =
     reverse . mapMaybe (`IM.lookup` vertices2nId) $ topSort graph
@@ -61,3 +45,77 @@ topologicalSort expr@(mp, n) =
             (bringMaybeOut . mapBoth (`IM.lookup` nId2vertices))
             exNodeEdges
     graph = buildG (0, IM.size mp - 1) verticesEdges
+
+-- | Get all the edges of the expressions
+--
+expressionEdges :: (ExpressionMap, Int) -> [(Int, Int)]
+expressionEdges (mp, n) = Set.toList $ edges n
+  where
+    edges :: Int -> Set (Int, Int)
+    edges nId =
+        let args = nodeArgs $ retrieveNode nId mp
+            thisNode = Set.fromList . map (nId, ) $ args
+         in Set.unions $ thisNode : map edges args
+
+-- | Mem map (offset, R or C, shape)
+--
+type MemMapEntry = (Int, EntryType, Shape)
+
+data MemMap =
+    MemMap
+        { entryMap :: IntMap MemMapEntry -- node id -> (offset, size)
+        , totalSize :: Int
+        }
+    deriving (Show, Eq, Ord)
+
+data EntryType
+    = EntryR
+    | EntryC
+    deriving (Show, Eq, Ord)
+
+data PartC
+    = Re
+    | Im
+    deriving (Show, Eq, Ord)
+
+data LocalOffset
+    = OffsetR [Int]
+    | OffsetC PartC [Int]
+    deriving (Show, Eq, Ord)
+
+-- | Make a memory map from an expression
+--
+makeMemMap :: NumType et => Expression d et -> MemMap
+makeMemMap expr@(Expression n mp) = uncurry MemMap $ foldl' f (IM.empty, 0) nIds
+  where
+    nIds = topologicalSort . unwrap $ expr
+    f :: (IntMap MemMapEntry, Int) -> Int -> (IntMap MemMapEntry, Int)
+    f (memMapSoFar, sizeSoFar) nId =
+        let (shape, node) = retrieveInternal nId mp
+            (nodeSz, mmShape)
+                | nodeElementType node mp == R = (product shape, EntryR)
+                | nodeElementType node mp == C = (2 * product shape, EntryC)
+            newMemMap = IM.insert nId (sizeSoFar, mmShape, shape) memMapSoFar
+         in (newMemMap, sizeSoFar + nodeSz)
+
+memOffset :: MemMap -> Int -> LocalOffset -> Int
+memOffset (MemMap entryMap _) nId lookupOffset
+    | Just (globalOffset, entryType, entryShape) <- IM.lookup nId entryMap =
+        case (entryType, lookupOffset) of
+            (EntryR, OffsetR indices) ->
+                globalOffset + computeInnerOffset entryShape indices
+            (EntryC, OffsetC Re indices) ->
+                globalOffset + computeInnerOffset (2 : entryShape) (0 : indices)
+            (EntryC, OffsetC Im indices) ->
+                globalOffset + computeInnerOffset (2 : entryShape) (1 : indices)
+    | otherwise = error "node id is not in the mem map"
+
+-- | Compute the inner offset:
+-- e.g computeInnerOffset [3, 4, 5] [2, 0, 1] = 2 * (4 * 5) + 0 * (5) + 1
+--
+computeInnerOffset :: [Int] -> [Int] -> Int
+computeInnerOffset shape indices
+    | length shape == length indices =
+        sum . zipWith (*) indices $ map product . tail . tails $ shape
+    | otherwise =
+        error "shape and indices are not compatible (not the same size)"
