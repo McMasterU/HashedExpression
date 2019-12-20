9973d33682174d0a65421e630972702535339ee1
Curtis D'Alves <curtis.dalves@gmail.com>
2019-12-20 14:57:36 -0500

    cleaned up after merge from CASCON2019 branch (removed alot)

--- Repeat.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#while  stack test --test-arguments '--match "/HashedToCSpec/Hashed To C spec/Evaluate hash interp should equal to C code evaluation (Expression One R)/"' --fast; do
-#    echo "sucess"
-#done
-while stack test --fast; do
-    echo
-done
