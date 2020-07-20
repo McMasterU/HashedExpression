91dc38a353cb76b6bbb91826d2d460427ec96751
Nhan Thai (dandoh) <dainhan605@gmail.com>
2020-07-20 11:45:29 +0700

    [ handle collision ] more strict property

--- test/CollisionSpec.hs
+++ test/CollisionSpec.hs
@@ -3,6 +3,8 @@ module CollisionSpec where
 import Control.Monad (foldM, forM_, replicateM)
 import Control.Monad.HT (nest)
 import qualified Data.IntMap.Strict as IM
+import Data.List (intercalate, sort)
+import qualified Data.Set as Set
 import HashedExpression.Internal
 import HashedExpression.Internal.Expression
 import HashedExpression.Internal.Hash
@@ -67,10 +69,11 @@ prop_safeMergeDiffShouldResolveCollision :: CollisionSample -> Expectation
 prop_safeMergeDiffShouldResolveCollision (CollisionSample contextMp accMp diff hashSpace) = do
   results <- replicateM 50 $ do
     dumbHash <- makeDumbHash hashSpace
-    let (mp, nID) = safeMergeDiff dumbHash contextMp accMp diff
-    return $ debugPrint (mp `IM.union` contextMp, nID)
-  -- Should preserve the diff's semantic regardless of hash scheme
-  -- All diffs should refer to the same expression
+    let (mp, _) = safeMergeDiff dumbHash contextMp accMp diff
+    let ids = IM.keys mp
+    -- All the subexpresisons as strings (thus only expression's structure and semantic matters, NodeIDs are irrelevant) concatenate
+    return $ intercalate "\n" $ Set.toList $ Set.fromList $ map (debugPrint . (mp `IM.union` contextMp,)) ids
+  -- Result should be the same regardless of how hashing played out
   forM_ (zip results (tail results)) $ \(x, y) -> do
     x @=? y
 
