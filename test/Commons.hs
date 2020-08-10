2f6d771b5fb0d799990defe2545a88b5cdf3cf63
Nhan Thai (dandoh) <dainhan605@gmail.com>
2020-08-10 21:53:53 +0700

    [ project & inject ] all to default

--- test/Commons.hs
+++ test/Commons.hs
@@ -76,15 +76,11 @@ genDouble = arbitrary `suchThat` inSmallRange
     inSmallRange x = x >= 0 && x <= 10
 
 -- |
-genValMap ::
-  forall size1D size2D1 size2D2.
-  (KnownNat size1D, KnownNat size2D1, KnownNat size2D2) =>
-  VarsAndParams ->
-  Gen ValMaps
+genValMap :: VarsAndParams -> Gen ValMaps
 genValMap vars = do
-  let sz1D = nat @size1D
-      sz2D1 = nat @size2D1
-      sz2D2 = nat @size2D2
+  let sz1D = nat @Default1D
+      sz2D1 = nat @Default2D1
+      sz2D2 = nat @Default2D2
   let [names0d, names1d, names2d, names3d] = vars
   list0d <- vectorOf (length names0d) genDouble
   let vm0 = Map.fromList . zip names0d $ map VScalar list0d
@@ -137,60 +133,44 @@ primitiveScalarC :: Gen (Expression Scalar C, VarsAndParams)
 primitiveScalarC = liftE2 (+:) <$> primitiveScalarR <*> primitiveScalarR
 
 -------------------------------------------------------------------------------
-primitive1DR ::
-  forall n.
-  KnownNat n =>
-  Gen (Expression n R, VarsAndParams)
+primitive1DR :: Gen (Expression Default1D R, VarsAndParams)
 primitive1DR = do
   varName <- elements . map (: "1") $ ['a' .. 'z']
   paramName <- elements . map (: "p1") $ ['a' .. 'z']
   dbl <- genDouble
   elements
-    [ (variable1D @n varName, [[], [varName], [], []]),
-      (param1D @n paramName, [[], [paramName], [], []]),
-      (constant1D @n dbl, [[], [], [], []])
+    [ (variable1D @Default1D varName, [[], [varName], [], []]),
+      (param1D @Default1D paramName, [[], [paramName], [], []]),
+      (constant1D @Default1D dbl, [[], [], [], []])
     ]
 
-primitive1DC ::
-  forall n.
-  KnownNat n =>
-  Gen (Expression n C, VarsAndParams)
-primitive1DC = liftE2 (+:) <$> primitive1DR @n <*> primitive1DR @n
+primitive1DC :: Gen (Expression Default1D C, VarsAndParams)
+primitive1DC = liftE2 (+:) <$> primitive1DR <*> primitive1DR 
 
 -------------------------------------------------------------------------------
-primitive2DR ::
-  forall m n.
-  (KnownNat m, KnownNat n) =>
-  Gen (Expression '(m, n) R, VarsAndParams)
+primitive2DR :: Gen (Expression '(Default2D1, Default2D2) R, VarsAndParams)
 primitive2DR = do
   varName <- elements . map (: "2") $ ['a' .. 'z']
   paramName <- elements . map (: "p2") $ ['a' .. 'z']
   dbl <- genDouble
   elements
-    [ (variable2D @m @n varName, [[], [], [varName], []]),
-      (param2D @m @n paramName, [[], [], [paramName], []]),
-      (constant2D @m @n dbl, [[], [], [], []])
+    [ (variable2D @Default2D1 @Default2D2 varName, [[], [], [varName], []]),
+      (param2D @Default2D1 @Default2D2 paramName, [[], [], [paramName], []]),
+      (constant2D @Default2D1 @Default2D2 dbl, [[], [], [], []])
     ]
 
-primitive2DC ::
-  forall m n.
-  (KnownNat m, KnownNat n) =>
-  Gen (Expression '(m, n) C, VarsAndParams)
-primitive2DC = liftE2 (+:) <$> primitive2DR @m @n <*> primitive2DR @m @n
+primitive2DC :: Gen (Expression '(Default2D1, Default2D2) C, VarsAndParams)
+primitive2DC = liftE2 (+:) <$> primitive2DR  <*> primitive2DR 
 
 -------------------------------------------------------------------------------
-genScalarR ::
-  forall default1D default2D1 default2D2.
-  (KnownNat default1D, KnownNat default2D1, KnownNat default2D2) =>
-  Int ->
-  Gen (Expression Scalar R, VarsAndParams)
+genScalarR :: Int -> Gen (Expression Scalar R, VarsAndParams)
 genScalarR size
   | size == 0 = primitiveScalarR
   | otherwise =
-    let sub = genScalarR @default1D @default2D1 @default2D2 (size `div` sizeReduceFactor)
-        subC = genScalarC @default1D @default2D1 @default2D2 (size `div` sizeReduceFactor)
-        sub1D = gen1DR @default1D @default2D1 @default2D2 (size `div` sizeReduceFactor)
-        sub2D = gen2DR @default1D @default2D1 @default2D2 (size `div` sizeReduceFactor)
+    let sub = genScalarR (size `div` sizeReduceFactor)
+        subC = genScalarC (size `div` sizeReduceFactor)
+        sub1D = gen1DR (size `div` sizeReduceFactor)
+        sub2D = gen2DR (size `div` sizeReduceFactor)
         fromPiecewise = do
           numBranches <- elements [2, 3]
           branches <- vectorOf numBranches sub
@@ -213,22 +193,19 @@ genScalarR size
             liftE1 xRe <$> subC,
             liftE1 xIm <$> subC,
             liftE2 (<.>) <$> sub1D <*> sub1D,
-            liftE2 (<.>) <$> sub2D <*> sub2D
+            liftE2 (<.>) <$> sub2D <*> sub2D,
+            oneof [liftE1 (project (at @3)) <$> sub1D]
           ]
 
 -------------------------------------------------------------------------------
-genScalarC ::
-  forall default1D default2D1 default2D2.
-  (KnownNat default1D, KnownNat default2D1, KnownNat default2D2) =>
-  Int ->
-  Gen (Expression Scalar C, VarsAndParams)
+genScalarC :: Int -> Gen (Expression Scalar C, VarsAndParams)
 genScalarC size
   | size == 0 = primitiveScalarC
   | otherwise =
-    let sub = genScalarC @default1D @default2D1 @default2D2 (size `div` sizeReduceFactor)
-        subR = genScalarR @default1D @default2D1 @default2D2 (size `div` sizeReduceFactor)
-        sub1D = gen1DC @default1D @default2D1 @default2D2 (size `div` sizeReduceFactor)
-        sub2D = gen2DC @default1D @default2D1 @default2D2 (size `div` sizeReduceFactor)
+    let sub = genScalarC (size `div` sizeReduceFactor)
+        subR = genScalarR (size `div` sizeReduceFactor)
+        sub1D = gen1DC (size `div` sizeReduceFactor)
+        sub2D = gen2DC (size `div` sizeReduceFactor)
         fromPiecewise = do
           numBranches <- elements [2, 3]
           branches <- vectorOf numBranches sub
@@ -254,17 +231,14 @@ genScalarC size
           ]
 
 -------------------------------------------------------------------------------
-gen1DR ::
-  forall n default2D1 default2D2.
-  (KnownNat n, KnownNat default2D1, KnownNat default2D2) =>
-  Int ->
-  Gen (Expression n R, VarsAndParams)
+gen1DR :: Int ->
+  Gen (Expression Default1D R, VarsAndParams)
 gen1DR size
   | size == 0 = primitive1DR
   | otherwise =
-    let sub = gen1DR @n @default2D1 @default2D2 (size `div` sizeReduceFactor)
-        subC = gen1DC @n @default2D1 @default2D2 (size `div` sizeReduceFactor)
-        subScalar = genScalarR @n @default2D1 @default2D2 (size `div` sizeReduceFactor)
+    let sub = gen1DR (size `div` sizeReduceFactor)
+        subC = gen1DC (size `div` sizeReduceFactor)
+        subScalar = genScalarR (size `div` sizeReduceFactor)
         fromPiecewise = do
           numBranches <- elements [2, 3]
           branches <- vectorOf numBranches sub
@@ -274,7 +248,7 @@ gen1DR size
               exp = piecewise marks (fst condition) $ map fst branches
           return (exp, vars)
         fromRotate = do
-          amount <- elements [- (nat @n) .. nat @n]
+          amount <- elements [- (nat @Default1D) .. nat @Default1D]
           liftE1 (rotate amount) <$> sub
         binary op = liftE2 op <$> sub <*> sub
         unary op = liftE1 op <$> sub
@@ -292,17 +266,13 @@ gen1DR size
           ]
 
 -------------------------------------------------------------------------------
-gen1DC ::
-  forall n default2D1 default2D2.
-  (KnownNat n, KnownNat default2D1, KnownNat default2D2) =>
-  Int ->
-  Gen (Expression n C, VarsAndParams)
+gen1DC :: Int -> Gen (Expression Default1D C, VarsAndParams)
 gen1DC size
   | size == 0 = primitive1DC
   | otherwise =
-    let sub = gen1DC @n @default2D1 @default2D2 (size `div` sizeReduceFactor)
-        subR = gen1DR @n @default2D1 @default2D2 (size `div` sizeReduceFactor)
-        subScalar = genScalarC @n @default2D1 @default2D2 (size `div` sizeReduceFactor)
+    let sub = gen1DC (size `div` sizeReduceFactor)
+        subR = gen1DR (size `div` sizeReduceFactor)
+        subScalar = genScalarC (size `div` sizeReduceFactor)
         fromPiecewise = do
           numBranches <- elements [2, 3]
           branches <- vectorOf numBranches sub
@@ -312,7 +282,7 @@ gen1DC size
               exp = piecewise marks (fst condition) $ map fst branches
           return (exp, vars)
         fromRotate = do
-          amount <- elements [- (nat @n) .. nat @n]
+          amount <- elements [- (nat @Default1D) .. nat @Default1D]
           liftE1 (rotate amount) <$> sub
         binary op = liftE2 op <$> sub <*> sub
         unary op = liftE1 op <$> sub
@@ -331,17 +301,13 @@ gen1DC size
           ]
 
 -------------------------------------------------------------------------------
-gen2DR ::
-  forall default1D m n.
-  (KnownNat default1D, KnownNat m, KnownNat n) =>
-  Int ->
-  Gen (Expression '(m, n) R, VarsAndParams)
+gen2DR :: Int -> Gen (Expression '(Default2D1, Default2D2) R, VarsAndParams)
 gen2DR size
   | size == 0 = primitive2DR
   | otherwise =
-    let sub = gen2DR @default1D @m @n (size `div` sizeReduceFactor)
-        subC = gen2DC @default1D @m @n (size `div` sizeReduceFactor)
-        subScalar = genScalarR @default1D @m @n (size `div` sizeReduceFactor)
+    let sub = gen2DR (size `div` sizeReduceFactor)
+        subC = gen2DC (size `div` sizeReduceFactor)
+        subScalar = genScalarR (size `div` sizeReduceFactor)
         fromPiecewise = do
           numBranches <- elements [2, 3]
           branches <- vectorOf numBranches sub
@@ -351,8 +317,8 @@ gen2DR size
               exp = piecewise marks (fst condition) $ map fst branches
           return (exp, vars)
         fromRotate = do
-          amount1 <- elements [- (nat @m) .. nat @m]
-          amount2 <- elements [- (nat @n) .. nat @n]
+          amount1 <- elements [- (nat @Default2D1) .. nat @Default2D1]
+          amount2 <- elements [- (nat @Default2D2) .. nat @Default2D2]
           liftE1 (rotate (amount1, amount2)) <$> sub
         binary op = liftE2 op <$> sub <*> sub
         unary op = liftE1 op <$> sub
@@ -370,17 +336,13 @@ gen2DR size
           ]
 
 -------------------------------------------------------------------------------
-gen2DC ::
-  forall default1D m n.
-  (KnownNat default1D, KnownNat m, KnownNat n) =>
-  Int ->
-  Gen (Expression '(m, n) C, VarsAndParams)
+gen2DC :: Int -> Gen (Expression '(Default2D1, Default2D2) C, VarsAndParams)
 gen2DC size
   | size == 0 = primitive2DC
   | otherwise =
-    let sub = gen2DC @default1D @m @n (size `div` sizeReduceFactor)
-        subR = gen2DR @default1D @m @n (size `div` sizeReduceFactor)
-        subScalar = genScalarC @default1D @m @n (size `div` sizeReduceFactor)
+    let sub = gen2DC (size `div` sizeReduceFactor)
+        subR = gen2DR (size `div` sizeReduceFactor)
+        subScalar = genScalarC (size `div` sizeReduceFactor)
         fromPiecewise = do
           numBranches <- elements [2, 3]
           branches <- vectorOf numBranches sub
@@ -390,8 +352,8 @@ gen2DC size
               exp = piecewise marks (fst condition) $ map fst branches
           return (exp, vars)
         fromRotate = do
-          amount1 <- elements [- (nat @m) .. nat @m]
-          amount2 <- elements [- (nat @n) .. nat @n]
+          amount1 <- elements [- (nat @Default2D1) .. nat @Default2D1]
+          amount2 <- elements [- (nat @Default2D2) .. nat @Default2D2]
           liftE1 (rotate (amount1, amount2)) <$> sub
         binary op = liftE2 op <$> sub <*> sub
         unary op = liftE1 op <$> sub
@@ -410,12 +372,12 @@ gen2DC size
           ]
 
 -------------------------------------------------------------------------------
-data Suite (size1D :: Nat) (size2D1 :: Nat) (size2D2 :: Nat) d et
+data Suite d et
   = Suite (Expression d et) ValMaps
   deriving (Show)
 
 -------------------------------------------------------------------------------
-type TestSuite = Suite Default1D Default2D1 Default2D2
+type TestSuite = Suite 
 
 type SuiteScalarR = TestSuite Scalar R
 
@@ -430,78 +392,60 @@ type SuiteTwoR = TestSuite '(Default2D1, Default2D2) R
 type SuiteTwoC = TestSuite '(Default2D1, Default2D2) C
 
 -------------------------------------------------------------------------------
-instance
-  (KnownNat size1D, KnownNat size2D1, KnownNat size2D2) =>
-  Arbitrary (Suite size1D size2D1 size2D2 Scalar R)
-  where
+instance Arbitrary SuiteScalarR where
   arbitrary = do
-    (exp, vars) <- sized $ genScalarR @size1D @size2D1 @size2D2
-    valMaps <- genValMap @size1D @size2D1 @size2D2 vars
+    (exp, vars) <- sized $ genScalarR 
+    valMaps <- genValMap  vars
     return $ Suite exp valMaps
 
-instance
-  (KnownNat size1D, KnownNat size2D1, KnownNat size2D2) =>
-  Arbitrary (Suite size1D size2D1 size2D2 Scalar C)
-  where
+instance Arbitrary SuiteScalarC where
   arbitrary = do
-    (exp, vars) <- sized $ genScalarC @size1D @size2D1 @size2D2
-    valMaps <- genValMap @size1D @size2D1 @size2D2 vars
+    (exp, vars) <- sized $ genScalarC 
+    valMaps <- genValMap  vars
     return $ Suite exp valMaps
 
-instance
-  (KnownNat size1D, KnownNat size2D1, KnownNat size2D2) =>
-  Arbitrary (Suite size1D size2D1 size2D2 size1D R)
-  where
+instance Arbitrary SuiteOneR where
   arbitrary = do
-    (exp, vars) <- sized $ gen1DR @size1D @size2D1 @size2D2
-    valMaps <- genValMap @size1D @size2D1 @size2D2 vars
+    (exp, vars) <- sized $ gen1DR 
+    valMaps <- genValMap  vars
     return $ Suite exp valMaps
 
-instance
-  (KnownNat size1D, KnownNat size2D1, KnownNat size2D2) =>
-  Arbitrary (Suite size1D size2D1 size2D2 size1D C)
-  where
+instance Arbitrary SuiteOneC where
   arbitrary = do
-    (exp, vars) <- sized $ gen1DC @size1D @size2D1 @size2D2
-    valMaps <- genValMap @size1D @size2D1 @size2D2 vars
+    (exp, vars) <- sized $ gen1DC 
+    valMaps <- genValMap  vars
     return $ Suite exp valMaps
 
-instance
-  (KnownNat size1D, KnownNat size2D1, KnownNat size2D2) =>
-  Arbitrary (Suite size1D size2D1 size2D2 '(size2D1, size2D2) R)
-  where
+instance Arbitrary SuiteTwoR where
   arbitrary = do
-    (exp, vars) <- sized $ gen2DR @size1D @size2D1 @size2D2
-    valMaps <- genValMap @size1D @size2D1 @size2D2 vars
+    (exp, vars) <- sized $ gen2DR 
+    valMaps <- genValMap  vars
     return $ Suite exp valMaps
 
-instance
-  (KnownNat size1D, KnownNat size2D1, KnownNat size2D2) =>
-  Arbitrary (Suite size1D size2D1 size2D2 '(size2D1, size2D2) C)
-  where
+instance Arbitrary SuiteTwoC where
   arbitrary = do
-    (exp, vars) <- sized $ gen2DC @size1D @size2D1 @size2D2
-    valMaps <- genValMap @size1D @size2D1 @size2D2 vars
+    (exp, vars) <- sized $ gen2DC 
+    valMaps <- genValMap  vars
     return $ Suite exp valMaps
 
 -------------------------------------------------------------------------------
 instance Arbitrary (Expression Scalar R) where
-  arbitrary = fst <$> sized (genScalarR @Default1D @Default2D1 @Default2D2)
+  arbitrary = fst <$> sized genScalarR
 
 instance Arbitrary (Expression Scalar C) where
-  arbitrary = fst <$> sized (genScalarC @Default1D @Default2D1 @Default2D2)
+  arbitrary = fst <$> sized genScalarC
 
-instance KnownNat n => Arbitrary (Expression n R) where
-  arbitrary = fst <$> sized (gen1DR @n @Default2D1 @Default2D2)
+instance Arbitrary (Expression Default1D R) where
+  arbitrary = fst <$> sized gen1DR
 
-instance KnownNat n => Arbitrary (Expression n C) where
-  arbitrary = fst <$> sized (gen1DC @n @Default2D1 @Default2D2)
+instance Arbitrary (Expression Default1D C) where
+  arbitrary = fst <$> sized gen1DC 
 
-instance (KnownNat m, KnownNat n) => Arbitrary (Expression '(m, n) R) where
-  arbitrary = fst <$> sized (gen2DR @Default1D @m @n)
+instance Arbitrary (Expression '(Default2D1, Default2D2) R) where
+  arbitrary = fst <$> sized gen2DR 
 
-instance (KnownNat m, KnownNat n) => Arbitrary (Expression '(m, n) C) where
-  arbitrary = fst <$> sized (gen2DC @Default1D @m @n)
+instance Arbitrary (Expression '(Default2D1, Default2D2) C) where
+  arbitrary = fst <$> sized gen2DC 
 
 -------------------------------------------------------------------------------
 data ArbitraryExpresion = forall d et. (Dimension d, ElementType et, Typeable et, Typeable d) => ArbitraryExpresion (Expression d et)
