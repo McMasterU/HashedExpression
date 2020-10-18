6d5057ec0c0bc650e35664cff7a214a535b00cf5
dandoh <dainhan605@gmail.com>
2020-10-18 15:22:52 +0700

    [ unit ] types and units

--- src/HashedExpression/Modeling/Unit.hs
+++ src/HashedExpression/Modeling/Unit.hs
@@ -1,1 +1,64 @@
+{-# LANGUAGE TypeOperators #-}
+
 module HashedExpression.Modeling.Unit where
+
+import GHC.TypeLits (KnownNat, Nat, Symbol, type (+))
+import HashedExpression.Internal.Base (ElementType (..), IsExpression (..), IsScalarReal (..))
+import qualified HashedExpression.Internal.Base as Base
+import HashedExpression.Internal.Builder (ExprBuilder, buildExpr)
+import HashedExpression.Modeling.Typed (D1, Scalar)
+import HashedExpression.Modeling.Unit.Types
+
+--------------------------------------------------------------------------------
+data DomainType = NormalDomain | FrequencyDomain
+
+type NORMAL_DOMAIN = 'NormalDomain
+
+type FREQUENCY_DOMAIN = 'FrequencyDomain
+
+--------------------------------------------------------------------------------
+data Frame = Frame Symbol DomainType
+
+type FRAME = 'Frame
+
+--------------------------------------------------------------------------------
+type family ListLength (xs :: [a]) :: Nat where
+  ListLength '[] = 0
+  ListLength (_ ': xs) = 1 + ListLength xs
+
+--------------------------------------------------------------------------------
+data
+  UnitExpr
+    (frame :: Frame)
+    (domainUnits :: [Unit])
+    (unit :: Unit)
+    (shape :: [Nat])
+    (et :: ElementType)
+  = ListLength domainUnits ~ ListLength shape => UnitExpr ExprBuilder
+
+instance IsExpression (UnitExpr frame domainUnit unit shape et) where
+  asRawExpr (UnitExpr exprBuilder) = buildExpr exprBuilder
+
+instance IsScalarReal (UnitExpr frame domainUnit unit Scalar R) where
+  asScalarRealRawExpr = asRawExpr
+
+constant :: Double -> UnitExpr frame domainUnits unit Scalar R
+constant = undefined
+
+constant1D :: (KnownNat n) => Double -> UnitExpr frame domainUnits unit (D1 n) R
+constant1D = undefined
+
+dft1D ::
+  UnitExpr (FRAME name NORMAL_DOMAIN) '[domainUnit] unit shape C ->
+  UnitExpr (FRAME name FREQUENCY_DOMAIN) '[Recip domainUnit] unit shape C
+dft1D = undefined
+
+dft2D ::
+  UnitExpr (FRAME name NORMAL_DOMAIN) '[domainUnit1, domainUnit2] unit '[m, n] C ->
+  UnitExpr (FRAME name FREQUENCY_DOMAIN) '[Recip domainUnit, Recip domainUnit2] unit '[m, n] C
+dft2D = undefined
+
+dft3D ::
+  UnitExpr (FRAME name NORMAL_DOMAIN) '[domainUnit1, domainUnit2, domainUnit3] unit '[m, n, p] C ->
+  UnitExpr (FRAME name FREQUENCY_DOMAIN) '[Recip domainUnit, Recip domainUnit2, Recip domainUnit3] unit '[m, n, p] C
+dft3D = undefined
---
+++ src/HashedExpression/Modeling/Unit/Types.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE TypeOperators #-}
+
+module HashedExpression.Modeling.Unit.Types where
+
+import GHC.TypeLits (Nat)
+import Numeric.NumType.DK.Integers (TypeInt (..), type (+), type (-))
+import qualified Numeric.NumType.DK.Integers as I
+
+type family NEG (n :: Nat) :: TypeInt where
+  NEG 0 = Zero
+  NEG 1 = Neg1
+  NEG 2 = Neg2
+  NEG 3 = Neg3
+  NEG 4 = Neg4
+  NEG 5 = Neg5
+  NEG 6 = Neg6
+  NEG 7 = Neg7
+  NEG 8 = Neg8
+  NEG 9 = Neg9
+  NEG n = Neg10Minus n
+
+type family POS (n :: Nat) :: TypeInt where
+  POS 0 = Zero
+  POS 1 = Pos1
+  POS 2 = Pos2
+  POS 3 = Pos3
+  POS 4 = Pos4
+  POS 5 = Pos5
+  POS 6 = Pos6
+  POS 7 = Pos7
+  POS 8 = Pos8
+  POS 9 = Pos9
+  POS n = Pos10Plus n
+
+data Unit
+  = Unit
+      TypeInt -- (length, m - meter)
+      TypeInt -- (mass, kg - kilogram)
+      TypeInt -- (time, s - second)
+      TypeInt -- (electric current, A - ampere)
+      TypeInt -- (thermodynamic temperature, K - Kevin)
+      TypeInt -- (amount of substance, mol - mole)
+      TypeInt -- (luminous intensity, cd - candela)
+
+type Unitless = 'Unit Zero Zero Zero Zero Zero Zero Zero
+
+--------------------------------------------------------------------------------
+infixr 8 |^|
+
+infixl 7 |*|, |/|
+
+type family (a :: Unit) |*| (b :: Unit) :: Unit where
+  Unitless |*| d = d
+  d |*| Unitless = d
+  ( 'Unit l m t i th n j) |*| ( 'Unit l' m' t' i' th' n' j') =
+    'Unit (l + l') (m + m') (t + t') (i + i') (th + th') (n + n') (j + j')
+
+type family (a :: Unit) |/| (b :: Unit) :: Unit where
+  d |/| Unitless = d
+  d |/| d = Unitless
+  ( 'Unit l m t i th n j) |/| ( 'Unit l' m' t' i' th' n' j') =
+    'Unit (l - l') (m - m') (t - t') (i - i') (th - th') (n - n') (j - j')
+
+type Recip (d :: Unit) = Unitless |/| d
+
+type family (d :: Unit) |^| (x :: TypeInt) where
+  Unitless |^| _ = Unitless
+  _ |^| 'Zero = Unitless
+  d |^| 'Pos1 = d
+  ( 'Unit l m t i th n j) |^| x =
+    'Unit (l I.* x) (m I.* x) (t I.* x) (i I.* x) (th I.* x) (n I.* x) (j I.* x)
+
+--------------------------------------------------------------------------------
+type Meter = 'Unit Pos1 Zero Zero Zero Zero Zero Zero
+
+--------------------------------------------------------------------------------
+type Kilogram = 'Unit Zero Pos1 Zero Zero Zero Zero Zero
+
+--------------------------------------------------------------------------------
+type Second = 'Unit Zero Zero Pos1 Zero Zero Zero Zero
+
+--------------------------------------------------------------------------------
+type Ampere = 'Unit Zero Zero Zero Pos1 Zero Zero Zero
+
+--------------------------------------------------------------------------------
+type Kevin = 'Unit Zero Zero Zero Zero Pos1 Zero Zero
+
+--------------------------------------------------------------------------------
+type Mole = 'Unit Zero Zero Zero Zero Zero Pos1 Zero
+
+--------------------------------------------------------------------------------
+type Candela = 'Unit Zero Zero Zero Zero Zero Zero Pos1
