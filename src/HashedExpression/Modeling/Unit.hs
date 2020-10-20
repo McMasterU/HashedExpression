fb3f56511f7f6a6a217a02fd1d556f554d0d55da
dandoh <dainhan605@gmail.com>
2020-10-20 20:01:32 +0700

    [ unit ] updates.

--- src/HashedExpression/Modeling/Unit.hs
+++ src/HashedExpression/Modeling/Unit.hs
@@ -1,13 +1,17 @@
+{-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE TypeOperators #-}
 
 module HashedExpression.Modeling.Unit where
 
 import GHC.TypeLits (KnownNat, Nat, Symbol, type (+))
-import HashedExpression.Internal.Base (ElementType (..), IsExpression (..), IsScalarReal (..))
+import HashedExpression.Internal.Base (ElementType (..), IsExpression (..), IsScalarReal (..), Op (..))
 import qualified HashedExpression.Internal.Base as Base
 import HashedExpression.Internal.Builder (ExprBuilder, buildExpr)
-import HashedExpression.Modeling.Typed (Scalar)
+import HashedExpression.Internal.MonadExpression
+import HashedExpression.Modeling.Typed (IsShape (..), Scalar)
 import HashedExpression.Modeling.Unit.Types
+import Prelude hiding ((*), (+), (/))
+import qualified Prelude as P
 
 --------------------------------------------------------------------------------
 data DomainType = NormalDomain | FrequencyDomain
@@ -17,48 +21,114 @@ type NORMAL_DOMAIN = 'NormalDomain
 type FREQUENCY_DOMAIN = 'FrequencyDomain
 
 --------------------------------------------------------------------------------
-data Frame = Frame Symbol DomainType
+data Frame = Frameless | Frame Symbol DomainType [Unit]
 
 type FRAME = 'Frame
+
+type FRAMELESS = 'Frameless
+
+type FRAME_1D name unit = FRAME name NORMAL_DOMAIN '[unit]
+
+type FRAME_2D name unit1 unit2 = FRAME name NORMAL_DOMAIN '[unit1, unit2]
+
+type FRAME_3D name unit1 unit2 unit3 = FRAME name NORMAL_DOMAIN '[unit1, unit2, unit3]
 
 --------------------------------------------------------------------------------
 type family ListLength (xs :: [a]) :: Nat where
   ListLength '[] = 0
   ListLength (_ ': xs) = 1 + ListLength xs
 
+type family FrameDimensionLength (frame :: Frame) :: Nat where
+  FrameDimensionLength FRAMELESS = 0
+  FrameDimensionLength (FRAME _ _ frameUnits) = ListLength frameUnits
+
+type FrameShapeMatched frame shape = FrameDimensionLength frame ~ ListLength shape
+
 --------------------------------------------------------------------------------
 data
   UnitExpr
     (frame :: Frame)
-    (domainUnits :: [Unit])
     (unit :: Unit)
     (shape :: [Nat])
     (et :: ElementType)
-  = ListLength domainUnits ~ ListLength shape => UnitExpr ExprBuilder
+  = UnitExpr ExprBuilder
 
-instance IsExpression (UnitExpr frame domainUnit unit shape et) where
+instance IsExpression (UnitExpr frame unit shape et) where
   asRawExpr (UnitExpr exprBuilder) = buildExpr exprBuilder
 
-instance IsScalarReal (UnitExpr frame domainUnit unit Scalar R) where
+instance IsScalarReal (UnitExpr frame unit Scalar R) where
   asScalarRealRawExpr = asRawExpr
 
-constant :: Double -> UnitExpr frame domainUnits unit Scalar R
-constant = undefined
+gvariable ::
+  forall frame unit shape.
+  (IsShape shape, FrameShapeMatched frame shape) =>
+  String ->
+  UnitExpr frame unit shape R
+gvariable name = UnitExpr $ introduceNode (toShape @shape, R, Var name)
 
-constant1D :: (KnownNat n) => Double -> UnitExpr frame domainUnits unit '[n] R
-constant1D = undefined
+gparam ::
+  forall frame unit shape.
+  (IsShape shape, FrameShapeMatched frame shape) =>
+  String ->
+  UnitExpr frame unit shape R
+gparam name = UnitExpr $ introduceNode (toShape @shape, R, Param name)
+
+gconstant ::
+  forall frame unit shape.
+  (IsShape shape, FrameShapeMatched frame shape) =>
+  Double ->
+  UnitExpr frame unit shape R
+gconstant val = UnitExpr $ introduceNode (toShape @shape, R, Const val)
+
+constant :: forall unit. Double -> UnitExpr FRAMELESS unit Scalar R
+constant = gconstant @FRAMELESS @unit @Scalar
+
+constant1D :: forall frame unit n. (KnownNat n) => FrameShapeMatched frame '[n] => Double -> UnitExpr frame unit '[n] R
+constant1D = gconstant @frame @unit @'[n]
+
+constant2D ::
+  forall frame unit m n.
+  (KnownNat m, KnownNat n) =>
+  FrameShapeMatched frame '[m, n] =>
+  Double ->
+  UnitExpr frame unit '[m, n] R
+constant2D = gconstant @frame @unit @'[m, n]
+
+(+) :: UnitExpr frame unit shape et -> UnitExpr frame unit shape et -> UnitExpr frame unit shape et
+(+) = binary (P.+)
+
+(*) :: UnitExpr frame unit1 shape et -> UnitExpr frame unit2 shape et -> UnitExpr frame (unit1 |*| unit2) shape et
+(*) = binary (P.*)
+
+(/) :: UnitExpr frame unit1 shape et -> UnitExpr frame unit2 shape et -> UnitExpr frame (unit1 |/| unit2) shape et
+(/) = binary (P./)
 
 dft1D ::
-  UnitExpr (FRAME name NORMAL_DOMAIN) '[domainUnit] unit shape C ->
-  UnitExpr (FRAME name FREQUENCY_DOMAIN) '[Recip domainUnit] unit shape C
-dft1D = undefined
+  UnitExpr (FRAME name NORMAL_DOMAIN '[frameUnit]) unit shape C ->
+  UnitExpr (FRAME name FREQUENCY_DOMAIN '[Recip frameUnit]) unit shape C
+dft1D = unary Base.ft
 
 dft2D ::
-  UnitExpr (FRAME name NORMAL_DOMAIN) '[domainUnit1, domainUnit2] unit '[m, n] C ->
-  UnitExpr (FRAME name FREQUENCY_DOMAIN) '[Recip domainUnit, Recip domainUnit2] unit '[m, n] C
-dft2D = undefined
+  UnitExpr (FRAME name NORMAL_DOMAIN '[frameUnit1, frameUnit2]) unit '[m, n] C ->
+  UnitExpr (FRAME name FREQUENCY_DOMAIN '[Recip frameUnit, Recip frameUnit2]) unit '[m, n] C
+dft2D = unary Base.ft
 
 dft3D ::
-  UnitExpr (FRAME name NORMAL_DOMAIN) '[domainUnit1, domainUnit2, domainUnit3] unit '[m, n, p] C ->
-  UnitExpr (FRAME name FREQUENCY_DOMAIN) '[Recip domainUnit, Recip domainUnit2, Recip domainUnit3] unit '[m, n, p] C
-dft3D = undefined
+  UnitExpr (FRAME name NORMAL_DOMAIN '[frameUnit1, frameUnit2, frameUnit3]) unit '[m, n, p] C ->
+  UnitExpr (FRAME name FREQUENCY_DOMAIN '[Recip frameUnit, Recip frameUnit2, Recip frameUnit3]) unit '[m, n, p] C
+dft3D = unary Base.ft
+
+--------------------------------------------------------------------------------
+
+-- | INTERNAL USE ONLY !
+unary :: (ExprBuilder -> ExprBuilder) -> UnitExpr frame1 unit1 shape1 et1 -> UnitExpr frame2 unit2 shape2 et2
+unary f (UnitExpr e) = UnitExpr $ f e
+
+binary ::
+  (ExprBuilder -> ExprBuilder -> ExprBuilder) ->
+  UnitExpr frame1 unit1 shape1 et1 ->
+  UnitExpr frame2 unit2 shape2 et2 ->
+  UnitExpr frame3 unit3 shape3 et3
+binary f (UnitExpr e1) (UnitExpr e2) = UnitExpr $ f e1 e2
+
+--------------------------------------------------------------------------------
--- src/HashedExpression/Modeling/Unit/Types.hs
+++ src/HashedExpression/Modeling/Unit/Types.hs
@@ -6,43 +6,67 @@ import GHC.TypeLits (Nat)
 import Numeric.NumType.DK.Integers (TypeInt (..), type (+), type (-))
 import qualified Numeric.NumType.DK.Integers as I
 
-type family NEG (n :: Nat) :: TypeInt where
-  NEG 0 = Zero
-  NEG 1 = Neg1
-  NEG 2 = Neg2
-  NEG 3 = Neg3
-  NEG 4 = Neg4
-  NEG 5 = Neg5
-  NEG 6 = Neg6
-  NEG 7 = Neg7
-  NEG 8 = Neg8
-  NEG 9 = Neg9
-  NEG n = Neg10Minus n
+-- type family NEG (n :: Nat) :: TypeInt where
+--   NEG 0 = Zero
+--   NEG 1 = Neg1
+--   NEG 2 = Neg2
+--  NEG 3 = Neg3
+--   NEG 4 = Neg4
+--   NEG 5 = Neg5
+--   NEG 6 = Neg6
+--   NEG 7 = Neg7
+--   NEG 8 = Neg8
+--   NEG 9 = Neg9
+--   NEG n = Neg10Minus n
 
-type family POS (n :: Nat) :: TypeInt where
-  POS 0 = Zero
-  POS 1 = Pos1
-  POS 2 = Pos2
-  POS 3 = Pos3
-  POS 4 = Pos4
-  POS 5 = Pos5
-  POS 6 = Pos6
-  POS 7 = Pos7
-  POS 8 = Pos8
-  POS 9 = Pos9
-  POS n = Pos10Plus n
+-- type family POS (n :: Nat) :: TypeInt where
+--   POS 0 = Zero
+--   POS 1 = Pos1
+--   POS 2 = Pos2
+--   POS 3 = Pos3
+--   POS 4 = Pos4
+--   POS 5 = Pos5
+--   POS 6 = Pos6
+--   POS 7 = Pos7
+--   POS 8 = Pos8
+--   POS 9 = Pos9
+--   POS n = Pos10Plus n
+
+newtype UMeter = Meter TypeInt
+
+newtype UKilogram = Kilogram TypeInt
+
+newtype USecond = Second TypeInt
+
+newtype UAmpere = Ampere TypeInt
+
+newtype UKevin =Kevin TypeInt
+
+newtype UMole = Mole TypeInt
+
+newtype UCandela = Candela TypeInt
 
 data Unit
   = Unit
-      TypeInt -- (length, m - meter)
-      TypeInt -- (mass, kg - kilogram)
-      TypeInt -- (time, s - second)
-      TypeInt -- (electric current, A - ampere)
-      TypeInt -- (thermodynamic temperature, K - Kevin)
-      TypeInt -- (amount of substance, mol - mole)
-      TypeInt -- (luminous intensity, cd - candela)
+      UMeter -- (length, m - meter)
+      UKilogram -- (mass, kg - kilogram)
+      USecond -- (time, s - second)
+      UAmpere -- (electric current, A - ampere)
+      UKevin -- (thermodynamic temperature, K - 'Kevin)
+      UMole -- (amount of substance, mol - mole)
+      UCandela -- (luminous intensity, cd - candela)
 
-type Unitless = 'Unit Zero Zero Zero Zero Zero Zero Zero
+data IntT = Pos Nat | Neg Nat
+
+type Unitless =
+  'Unit
+    ('Meter Zero)
+    ('Kilogram Zero)
+    ('Second Zero)
+    ('Ampere Zero)
+    ('Kevin Zero)
+    ('Mole Zero)
+    ('Candela Zero)
 
 --------------------------------------------------------------------------------
 infixr 8 |^|
@@ -52,14 +76,18 @@ infixl 7 |*|, |/|
 type family (a :: Unit) |*| (b :: Unit) :: Unit where
   Unitless |*| d = d
   d |*| Unitless = d
-  ('Unit l m t i th n j) |*| ('Unit l' m' t' i' th' n' j') =
-    'Unit (l + l') (m + m') (t + t') (i + i') (th + th') (n + n') (j + j')
+  ( 'Unit ('Meter l) ('Kilogram m) ('Second t) ('Ampere i) ('Kevin th) 
+            ('Mole n) ('Candela j))
+    |*| ( 'Unit ('Meter l') ('Kilogram m') 
+            ('Second t') ('Ampere i') ('Kevin th') ('Mole n') ('Candela j')) =
+    'Unit ('Meter (l + l')) ('Kilogram (m + m')) ('Second (t + t')) 
+            ('Ampere (i + i')) ('Kevin (th + th')) ('Mole (n + n')) ('Candela (j + j'))
 
 type family (a :: Unit) |/| (b :: Unit) :: Unit where
   d |/| Unitless = d
   d |/| d = Unitless
-  ('Unit l m t i th n j) |/| ('Unit l' m' t' i' th' n' j') =
-    'Unit (l - l') (m - m') (t - t') (i - i') (th - th') (n - n') (j - j')
+  ( 'Unit ('Meter l) ('Kilogram m) ('Second t) ('Ampere i) ('Kevin th) ('Mole n) ('Candela j)) |/| ( 'Unit ('Meter l') ('Kilogram m') ('Second t') ('Ampere i') ('Kevin th') ('Mole n') ('Candela j')) =
+    'Unit ('Meter (l - l')) ('Kilogram (m - m')) ('Second (t - t')) ('Ampere (i - i')) ('Kevin (th - th')) ('Mole (n - n')) ('Candela (j - j'))
 
 type Recip (d :: Unit) = Unitless |/| d
 
@@ -67,26 +95,17 @@ type family (d :: Unit) |^| (x :: TypeInt) where
   Unitless |^| _ = Unitless
   _ |^| 'Zero = Unitless
   d |^| 'Pos1 = d
-  ('Unit l m t i th n j) |^| x =
-    'Unit (l I.* x) (m I.* x) (t I.* x) (i I.* x) (th I.* x) (n I.* x) (j I.* x)
+  ( 'Unit ('Meter l) ('Kilogram m) ('Second t) ('Ampere i) ('Kevin th) ('Mole n) ('Candela j)) |^| x =
+    'Unit ('Meter (l I.* x)) ('Kilogram (m I.* x)) ('Second (t I.* x)) ('Ampere (i I.* x)) 
+        ('Kevin (th I.* x)) ('Mole (n I.* x)) ('Candela (j I.* x))
 
 --------------------------------------------------------------------------------
-type Meter = 'Unit Pos1 Zero Zero Zero Zero Zero Zero
+type Meter = 'Unit ('Meter Pos1) ('Kilogram Zero) ('Second Zero) ('Ampere Zero) ('Kevin Zero) ('Mole Zero) ('Candela Zero)
+type Kilogram = 'Unit ('Meter Zero) ('Kilogram Pos1) ('Second Zero) ('Ampere Zero) ('Kevin Zero) ('Mole Zero) ('Candela Zero)
+type Second = 'Unit ('Meter Zero) ('Kilogram Zero) ('Second Pos1) ('Ampere Zero) ('Kevin Zero) ('Mole Zero) ('Candela Zero)
+type Ampere = 'Unit ('Meter Zero) ('Kilogram Zero) ('Second Zero) ('Ampere Pos1) ('Kevin Zero) ('Mole Zero) ('Candela Zero)
+type Kevin = 'Unit ('Meter Zero) ('Kilogram Zero) ('Second Zero) ('Ampere Zero) ('Kevin Pos1) ('Mole Zero) ('Candela Zero)
+type Mole = 'Unit ('Meter Zero) ('Kilogram Zero) ('Second Zero) ('Ampere Zero) ('Kevin Zero) ('Mole Pos1) ('Candela Zero)
+type Candela = 'Unit ('Meter Zero) ('Kilogram Zero) ('Second Zero) ('Ampere Zero) ('Kevin Zero) ('Mole Zero) ('Candela Pos1)
 
 --------------------------------------------------------------------------------
-type Kilogram = 'Unit Zero Pos1 Zero Zero Zero Zero Zero
-
---------------------------------------------------------------------------------
-type Second = 'Unit Zero Zero Pos1 Zero Zero Zero Zero
-
---------------------------------------------------------------------------------
-type Ampere = 'Unit Zero Zero Zero Pos1 Zero Zero Zero
-
---------------------------------------------------------------------------------
-type Kevin = 'Unit Zero Zero Zero Zero Pos1 Zero Zero
-
---------------------------------------------------------------------------------
-type Mole = 'Unit Zero Zero Zero Zero Zero Pos1 Zero
-
---------------------------------------------------------------------------------
-type Candela = 'Unit Zero Zero Zero Zero Zero Zero Pos1
